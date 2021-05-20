2bed8d251cfd29f6f749b3348a7ce55866531b65
JuanFernandezJubin <fernandezjubin.data@gmail.com>
2021-05-20 11:37:05 -0300

    architecture of the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListadoNoticiasComponent,
+    NavbarComponent,
+    SpinnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/formulario/formulario.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+
+  public countrySelected : string = 'ar';
+  public categorySelected : string = 'general';
+
+  public countries: any[] = [
+    { value: 'ar', name: 'Argentina' },
+    { value: 'br', name: 'Brasil' },
+    { value: 'fr', name: 'Francia' },
+    { value: 'hu', name: 'Hungría' },
+    { value: 'mx', name: 'México' },
+    { value: 'gb', name: 'Reino Unido' },
+  ];
+  public categories: any[] = [
+    { value: 'general', name: 'General' },
+    { value: 'business', name: 'Negocios' },
+    { value: 'entertaiment', name: 'Entretenimiento' },
+    { value: 'health', name: 'Salud' },
+    { value: 'science', name: 'Ciencia' },
+    { value: 'sports', name: 'Deportes' },
+    { value: 'technology', name: 'Tecnología' },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  searchNews() {
+    console.log(this.categorySelected);
+    console.log(this.countrySelected);
+  }
+
+}
